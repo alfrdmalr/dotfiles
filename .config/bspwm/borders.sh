3c898cc4b40bc0abd79f6ab49bb9652e91b7c152
alfrdmalr <alfrdmalr@gmail.com>
2021-12-27 11:24:24 -0500

    update theming/compositor options

--- .config/bspwm/borders.sh
+++ .config/bspwm/borders.sh
@@ -4,6 +4,7 @@ wal-color() {
   (cat ~/.cache/wal/colors.json) | grep -e $1 | awk '{print substr($2, 2, 7)}'
 }
 
+bspc config border_width    05
 bspc config normal_border_color $(wal-color 'color0')
 bspc config focused_border_color $(wal-color 'color2')
 bspc config active_border_color $(wal-color 'color0')
--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -2,6 +2,8 @@
 
 killall -q polybar
 
+#while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
 for m in $(polybar --list-monitors | cut -d":" -f1); do
   MONITOR=$m polybar --reload alfbar &
 done  

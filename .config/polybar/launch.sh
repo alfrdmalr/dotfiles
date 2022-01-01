642ca6faf5b6f1c7261cbb59d1c9a9c124b5745c
Alfred Maler <alfrdmalr@gmail.com>
2022-01-01 09:14:00 -0500

    init commit for jasmine

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
+# Terminate already running bar instances
 killall -q polybar
 
-#while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-  MONITOR=$m polybar --reload alfbar &
-done  
-
-echo "Bars launched..."
+# Launch Polybar
+polybar top -c ~/.config/polybar/config.ini &
